a2947f7557fe3cbe24526c8245ec250d252347d4
SIELINOU NOUBISSIE ERIC ROMUALD <sielinou1605@gmail.com>
2023-03-24 18:54:52 +0100

    deployment ok

---
+++ bill-app/Billed-app-FR-Front/src/containers/Bills.js
@@ -0,0 +1,60 @@
+import { ROUTES_PATH } from '../constants/routes.js'
+import { formatDate, formatStatus } from "../app/format.js"
+import Logout from "./Logout.js"
+
+export default class {
+  constructor({ document, onNavigate, store, localStorage }) {
+    this.document = document
+    this.onNavigate = onNavigate
+    this.store = store
+    const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
+    if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
+    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
+    if (iconEye) iconEye.forEach(icon => {
+      icon.addEventListener('click', () => this.handleClickIconEye(icon))
+    })
+    new Logout({ document, localStorage, onNavigate })
+  }
+
+  handleClickNewBill = () => {
+    this.onNavigate(ROUTES_PATH['NewBill'])
+  }
+
+  handleClickIconEye = (icon) => {
+    const billUrl = icon.getAttribute("data-bill-url")
+    const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
+    $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
+    $('#modaleFile').modal('show')
+  }
+
+  getBills = () => {
+    if (this.store) {
+      return this.store
+      .bills()
+      .list()
+      .then(snapshot => {
+        const bills = snapshot
+          .map(doc => {
+            try {
+              return {
+                ...doc,
+                date: formatDate(doc.date),
+                status: formatStatus(doc.status)
+              }
+            } catch(e) {
+                            // si, pour une raison quelconque, des données corrompues ont été introduites,
+                           //  nous gérons ici la fonction formatDate défaillante consigner l’erreur et renvoyer la date non formatée dans ce cas
+              console.log(e,'for',doc)
+              return {
+                ...doc,
+                date: doc.date,
+                status: formatStatus(doc.status)
+              }
+            }
+          })
+          console.log('length', bills.length)
+        return bills
+      })
+    }
+  }
+}
